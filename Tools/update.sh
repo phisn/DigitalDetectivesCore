73ba71248c934de6b4a277d6905f1c066aab4d40
Philipp Hinz <phisn@outlook.de>
2021-02-15 01:12:01 +0100

    fixed publish "update" script and updated solution config

--- Tools/update.sh
+++ Tools/update.sh
@@ -1,3 +1,3 @@
 sudo docker pull localhost:5000/server
-sudo docker run --name piserver -d --privileged -v /dev:/dev localhost:5000/server
+sudo docker run --name piserver -d --privileged -v /sys/class/thermal/thermal_zone0:/sys/class/thermal/thermal_zone0 -v /dev:/dev localhost:5000/server
 exit 0
