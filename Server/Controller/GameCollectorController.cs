0b16ff815817cffefb0baf93bb071d61018c8c5f
Philipp Hinz <phisn@outlook.de>
2021-02-15 01:10:56 +0100

    added empty controllers for all game states

---
+++ Server/Controller/GameCollectorController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Server.Attributes;
+using Server.Game;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [GameState(State.Collect)]
+    public class GameCollectorController : ControllerBase
+    {
+    }
+}
