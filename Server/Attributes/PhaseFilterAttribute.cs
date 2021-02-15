b2fcd8f50d4c974765261d7b86d2f0ec58fcc32f
Philipp Hinz <phisn@outlook.de>
2021-02-15 01:05:54 +0100

    added Phase and PhaseFilter

---
+++ Server/Attributes/PhaseFilterAttribute.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+using Server.Game;
+using Server.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Attributes
+{
+    public class PhaseFilterAttribute : ActionFilterAttribute
+    {
+
+        public PhaseFilterAttribute(Phase phase)
+        {
+            this.Phase = phase;
+        }
+
+        public Phase Phase { get; private set; }
+
+        public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            IGameStateService gameState = context.HttpContext.RequestServices.GetService<IGameStateService>();
+
+            if (gameState.CurrentPhase != Phase)
+            {
+                context.Result = new BadRequestResult();
+                return;
+            }
+
+            await next();
+        }
+    }
+}
---
+++ Server/Models/Phase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Game
+{
+    public enum Phase
+    {
+        Collect,
+        Setup,
+        Running
+    }
+}
