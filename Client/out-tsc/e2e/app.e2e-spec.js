2db767971fd8620cba07509da0d7255f024462af
Philipp Hinz <phisn@outlook.de>
2021-03-14 14:05:24 +0100

    updated client config

---
+++ Client/out-tsc/e2e/app.e2e-spec.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const app_po_1 = require("./app.po");
+describe('new App', () => {
+    let page;
+    beforeEach(() => {
+        page = new app_po_1.AppPage();
+    });
+    it('should be blank', () => {
+        page.navigateTo();
+        expect(page.getParagraphText()).toContain('Start with Ionic UI Components');
+    });
+});
+//# sourceMappingURL=app.e2e-spec.js.map
