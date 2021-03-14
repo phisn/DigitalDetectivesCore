16972483a89053f6da98fe7306ef619f2939f9d9
Philipp Hinz <phisn@outlook.de>
2021-03-14 14:02:16 +0100

    added empty gamecontroller for api access

---
+++ Server/Application/Controller/GameController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Application.Controller
+{
+    [ApiController]
+    public class GameController : ControllerBase
+    {
+        public IActionResult Start()
+        {
+
+
+            return Ok();
+        }
+    }
+}
