5825dbc17d77540c881dcfcc3d40389b05a2ae47
Philipp Hinz <phisn@outlook.de>
2021-03-18 22:53:34 +0100

    removed gamestateservice and added ingameservice for match state

---
+++ Server/Application/Services/IIngameService.cs
@@ -0,0 +1,16 @@
+﻿using Server.Game.Models.Match;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Application.Services
+{
+    public interface IIngameService
+    {
+        public Match Match { get; }
+
+        public void StartMatch(Match match);
+        public void CancelMatch();
+    }
+}
---
+++ Server/Application/Services/IngameService.cs
@@ -0,0 +1,26 @@
+﻿using Server.Game.Models.Match;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Application.Services
+{
+    public class IngameService : IIngameService
+    {
+        public Match Match { get; private set; }
+
+        public void StartMatch(Match match)
+        {
+            if (Match != null)
+                throw new Exception("Match already running");
+
+            Match = match;
+        }
+
+        public void CancelMatch()
+        {
+            Match = null;
+        }
+    }
+}
