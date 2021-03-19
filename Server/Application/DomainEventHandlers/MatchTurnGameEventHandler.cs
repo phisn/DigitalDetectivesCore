57878c01fcf0ff19d2815dfa08427b893bf9aabf
Philipp Hinz <phisn@outlook.de>
2021-03-19 14:24:22 +0100

    renamed TurnGameEventHandler and implemented it

---
+++ Server/Application/DomainEventHandlers/MatchTurnGameEventHandler.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.SignalR;
+using Server.Application.Hubs;
+using Server.Application.Services;
+using Server.Game.Events;
+using Server.Game.Models.Match;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server.Application.DomainEventHandlers
+{
+    public class MatchTurnGameEventHandler : INotificationHandler<MatchTurnGameEvent>
+    {
+        public MatchTurnGameEventHandler(
+            IHubContext<IngameHub, IIngameHubClient> ingameHub, 
+            IIngameService ingameService)
+        {
+            this.ingameHub = ingameHub;
+            this.ingameService = ingameService;
+        }
+
+        public async Task Handle(MatchTurnGameEvent notification, CancellationToken cancellationToken)
+        {
+            foreach ((Guid userID, Player player) in ingameService.Registered)
+            {
+                await ingameHub.Clients.Group(userID.ToString()).UpdateStateByPlayer(player);
+            }
+        }
+
+        private IHubContext<IngameHub, IIngameHubClient> ingameHub;
+        private IIngameService ingameService;
+
+    }
+}
